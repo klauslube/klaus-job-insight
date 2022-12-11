d8a7c8e484be28e3935eeb024ed04b7df9d02ec5
klauslube <klauslube93@gmail.com>
2022-12-11 19:03:00 -0300

    feat: implement read file jobs

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,9 +1,17 @@
 from functools import lru_cache
 from typing import List, Dict
+import csv
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
+    with open(path) as csv_file:
+        jobs = csv.DictReader(csv_file)
+        jobs_list = []
+        for job in jobs:
+            jobs_list.append(job)
+        return jobs_list
+        # return [job for job in jobs]  # compreensao de lista
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -18,7 +26,7 @@ def read(path: str) -> List[Dict]:
     """
     raise NotImplementedError
 
- 
+
 def get_unique_job_types(path: str) -> List[str]:
     """Checks all different job types and returns a list of them
 
