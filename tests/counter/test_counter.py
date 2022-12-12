e6edbb91b444c3e3181bf44e7f7510f6f963ddb4
klauslube <klauslube93@gmail.com>
2022-12-11 22:15:49 -0300

    test: implement counter words

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,6 +1,11 @@
+import pytest
 from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
     assert count_ocurrences("data/jobs.csv", "trybe") == 0
-    # assert count_ocurrences("data/jobs.csv", "") == 0
+    assert count_ocurrences("data/jobs.csv", "Javascript") == 122
+    assert count_ocurrences("data/jobs.csv", "JAVASCRIPT") == 122
+    # assert count_ocurrences("data/jobs.csv", "trybe") == 0
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("data/jobs.json", "java")
